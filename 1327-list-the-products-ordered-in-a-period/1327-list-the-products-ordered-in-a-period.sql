2b40317c2b21c910d05bed8183622fea3853f55a
Alisha Gonsalves <157180924+alu268work@users.noreply.github.com>
2025-05-01 13:31:47 -0500

    Time: 674 ms (94.61%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,10 +1,9 @@
-SELECT 
-p.product_name,
+SELECT p.product_name,
 SUM(o.unit) AS unit
 FROM Products p
 JOIN Orders o
 ON p.product_id = o.product_id
-WHERE o.order_date LIKE '2020-02%'
+WHERE order_date LIKE '2020-02%'
 GROUP BY p.product_name
-HAVING unit>=100
+HAVING SUM(o.unit)>= 100
 ;
