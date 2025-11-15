83468fb191a436ff7867de58be4ef6891bed8c2d
Alisha Gonsalves <157180924+alu268work@users.noreply.github.com>
2025-11-15 16:48:40 -0700

    Time: 766 ms (98.28%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,16 +1,16 @@
-# Write your MySQL query statement below
-# Write your MySQL query statement below
 WITH CTE AS
 (
-    SELECT *,
-    DENSE_RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) AS r
+    SELECT departmentId,
+    DENSE_RANK()OVER(PARTITION BY departmentId ORDER BY salary DESC) AS rank_sal,
+    salary,
+    name
     FROM Employee
 )
 SELECT d.name AS Department,
 c.name AS Employee,
-c.salary
+c.salary AS Salary
 FROM CTE c
 JOIN Department d
 ON c.departmentId = d.id
-WHERE r<=3
+WHERE c.rank_sal <= 3
 ;
