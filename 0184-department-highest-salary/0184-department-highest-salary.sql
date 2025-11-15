bb7355d80349e6382e5343377375f09acaf44c58
Alisha Gonsalves <157180924+alu268work@users.noreply.github.com>
2025-11-15 16:47:18 -0700

    Time: 534 ms (99.08%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,15 +1,14 @@
-# Write your MySQL query statement below
 WITH CTE AS
 (
     SELECT *,
-    DENSE_RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) AS r
+    RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) AS rnk
     FROM Employee
 )
 SELECT d.name AS Department,
 c.name AS Employee,
-c.salary
+c.salary AS Salary
 FROM CTE c
 JOIN Department d
 ON c.departmentId = d.id
-WHERE r=1
+WHERE rnk = 1
 ;
