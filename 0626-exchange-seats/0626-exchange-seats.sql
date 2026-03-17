554af170e653599ca348355274b0fa9fba9e177b
Alisha Gonsalves <157180924+alu268work@users.noreply.github.com>
2026-03-17 16:09:41 -0600

    Time: 330 ms (65.55%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,7 +1,7 @@
 SELECT 
-CASE WHEN id%2 != 0 AND id IN (SELECT max(id) FROM Seat) THEN id
-WHEN id%2 = 0 THEN id -1
-ELSE id+1 END as id,
+CASE WHEN id = (SELECT MAX(id) FROM Seat) AND id%2 != 0 THEN id
+WHEN id%2 != 0 THEN id+1
+ELSE id-1 END AS id,
 student
 FROM Seat
 ORDER BY id
