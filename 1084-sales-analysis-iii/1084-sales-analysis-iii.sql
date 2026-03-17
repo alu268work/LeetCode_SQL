e5ea3fcc92ba4c1a1722e35a7db8a9f5c2392b55
Alisha Gonsalves <157180924+alu268work@users.noreply.github.com>
2026-03-17 16:27:30 -0600

    Time: 1052 ms (82.45%), Space: 0B (100%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,6 +1,11 @@
-SELECT 
-product_id, product_name
+SELECT product_id,
+product_name
 FROM Product
-WHERE product_id IN (SELECT product_id FROM Sales GROUP BY product_id HAVING MIN(sale_date) >= '2019-01-01'
-AND MAX(sale_date)<='2019-03-31')
+WHERE product_id IN (
+    SELECT product_id
+    FROM Sales
+    GROUP BY 1
+    HAVING MIN(sale_date) >= '2019-01-01'
+    AND MAX(sale_date) <= '2019-03-31'
+)
 ;
