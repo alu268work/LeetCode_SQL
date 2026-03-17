5658f7dbe8da50e06489474c38712e3f04c8f907
Alisha Gonsalves <157180924+alu268work@users.noreply.github.com>
2026-03-17 16:34:25 -0600

    Time: 497 ms (38.76%), Space: 0B (100%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,8 @@
+SELECT activity_date as day,
+count(DISTINCT user_id) as active_users
+FROM Activity
+WHERE activity_date BETWEEN
+      DATE_SUB('2019-07-27', INTERVAL 29 DAY)
+  AND '2019-07-27'
+GROUP BY 1
+;
